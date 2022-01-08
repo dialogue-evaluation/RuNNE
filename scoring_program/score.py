6471826fe1b31632385aabc45b7bc8766bd6b2b5
= <mzmey37@gmail.com>
2022-01-08 16:27:30 +0100

    new scoring script

--- scoring_program/score.py
+++ scoring_program/score.py
@@ -3,7 +3,7 @@ import os
 import json
 
 
-EXCLUDE_TYPE_INDS = [25, 41]
+FEW_SHOT_TYPES = ['DISEASE', 'PENALTY', 'WORK_OF_ART']
 
 
 def load_jsonl(in_path, subdir):
@@ -19,7 +19,7 @@ def load_jsonl(in_path, subdir):
 def load_ner_types(in_path, subdir):
     fname = os.path.join(in_path, subdir, 'ners.txt')
     with open(fname, 'r') as f:
-        return f.read().split('\n')
+        return [each.strip() for each in f.read().split('\n') if each.strip() != '']
 
 
 class Evaluator:
@@ -29,15 +29,18 @@ class Evaluator:
         self.ner_types = load_ner_types(in_path, 'ref')
         self.num_types = len(self.ner_types)
         self.ner_maps = {ner: (i + 1) for i, ner in enumerate(self.ner_types)}
+        self.few_shot_type_inds = [self.ner_maps[tname] - 1 for tname in FEW_SHOT_TYPES]
+
+    def validate(self):
+        for pred_example in self.pred_data:
+            for i, (s, e, t) in enumerate(pred_example['ners'], 1):
+                if t not in self.ner_maps:
+                    raise ValueError('{t} is unknown type name from line {i}'.format(t=t, i=i))
 
     def evaluate(self):
         tp, fn, fp = 0, 0, 0
         sub_tp, sub_fn, sub_fp = [0] * self.num_types, [0] * self.num_types, [0] * self.num_types
         for gold_example, pred_example in zip(self.eval_data, self.pred_data):
-            # gold_ners = set([(sid, s, e, self.ner_maps[t])
-            #                  for sid, ner in enumerate(gold_example['ners']) for s, e, t in ner])
-            # pred_ners = set([(sid, s, e, self.ner_maps[t])
-            #                  for sid, ner in enumerate(pred_example['ners']) for s, e, t in ner])
             gold_ners = set([(s, e, self.ner_maps[t]) for s, e, t in gold_example['ners']])
             pred_ners = set([(s, e, self.ner_maps[t]) for s, e, t in pred_example['ners']])
             tp += len(gold_ners & pred_ners)
@@ -55,23 +58,30 @@ class Evaluator:
         print("Mention F1: {:.2f}%".format(m_f1 * 100))
         print("Mention recall: {:.2f}%".format(m_r * 100))
         print("Mention precision: {:.2f}%".format(m_p * 100))
-        print("****************SUB NER TYPES********************")
         f1_scores_list = []
+        counts_list = []
         for i in range(self.num_types):
-            sub_r = 0 if sub_tp[i] == 0 else float(sub_tp[i]) / (sub_tp[i] + sub_fn[i])
-            sub_p = 0 if sub_tp[i] == 0 else float(sub_tp[i]) / (sub_tp[i] + sub_fp[i])
-            sub_f1 = 0 if sub_p == 0 else 2.0 * sub_r * sub_p / (sub_r + sub_p)
+            sub_r = 0. if sub_tp[i] == 0 else float(sub_tp[i]) / (sub_tp[i] + sub_fn[i])
+            sub_p = 0. if sub_tp[i] == 0 else float(sub_tp[i]) / (sub_tp[i] + sub_fp[i])
+            sub_f1 = 0. if sub_p == 0 else 2.0 * sub_r * sub_p / (sub_r + sub_p)
             f1_scores_list.append(sub_f1)
-            # print("{} F1: {:.2f}%".format(self.ner_types[i], sub_f1 * 100))
-            # print("{} recall: {:.2f}%".format(self.ner_types[i],sub_r * 100))
-            # print("{} precision: {:.2f}%".format(self.ner_types[i],sub_p * 100))
+            counts_list.append(sub_tp[i] + sub_fn[i])
         summary_dict = {}
         summary_dict["Mention F1"] = m_f1
         summary_dict["Mention recall"] = m_r
         summary_dict["Mention precision"] = m_p
-        summary_dict["Macro F1"] = sum([each for i, each in enumerate(f1_scores_list) if i not in EXCLUDE_TYPE_INDS]) \
-            / float(self.num_types)
-        return summary_dict, summary_dict["Macro F1"]
+        filtered_f1s = [
+            f1
+            for i, (f1, count) in enumerate(zip(f1_scores_list, counts_list))
+            if i not in self.few_shot_type_inds and count > 0
+        ]
+        summary_dict["Macro F1"] = sum(filtered_f1s) / float(len(filtered_f1s))
+        summary_dict["Macro F1 few-shot"] = sum(
+            [each for i, each in enumerate(f1_scores_list) if i in self.few_shot_type_inds]
+        ) / float(len(self.few_shot_type_inds))
+        print("Macro F1: {:.2f}%".format(summary_dict["Macro F1"] * 100))
+        print("Macro F1 few-shot: {:.2f}%".format(summary_dict["Macro F1 few-shot"] * 100))
+        return summary_dict, summary_dict["Macro F1"], summary_dict["Macro F1 few-shot"]
 
 
 def parse_args():
@@ -86,9 +96,11 @@ def main():
     evaluator = Evaluator(args.in_path)
     if not os.path.exists(args.out_path):
         os.mkdir(args.out_path)
-    _, f1 = evaluator.evaluate()
+    evaluator.validate()
+    _, f1, f1_fs = evaluator.evaluate()
     with open(os.path.join(args.out_path, 'scores.txt'), 'w') as f:
-        f.write('f1_score: {:.5f}'.format(f1))
+        f.write('f1_score: {:.5f}\n'.format(f1))
+        f.write('f1_score_few_shot: {:.5f}'.format(f1_fs))
 
 
 if __name__ == '__main__':
